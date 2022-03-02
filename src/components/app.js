b83074bae927de873ae343598c8bb7c8039ebaec
Alex Jones <ajonzy@gmail.com>
2022-03-01 22:02:09 -0700

    Added auth

--- src/components/app.js
+++ src/components/app.js
@@ -4,6 +4,7 @@ import { Switch, Route } from "react-router"
 import Home from "./pages/home"
 import Quote from "./pages/quote"
 import About from "./pages/about"
+import Auth from "./pages/auth"
 
 export default class App extends Component {
 render() {
@@ -13,6 +14,7 @@ render() {
           <Route exact path="/" component={Home} />
           <Route path="/quote" component={Quote} />
           <Route path="/about" component={About} />
+          <Route path="/auth" component={Auth} />
         </Switch>
       </div>
     );
---
+++ src/components/pages/auth.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react"
+
+import Navbar from "../navbar"
+import Footer from "../footer"
+
+export default class Auth extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            usernameInput: "",
+            passwordInput: ""
+        }
+
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault()
+
+        fetch("https://qotd-aoj.herokuapp.com/user/login", {
+            method: "POST",
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify({
+                username: this.state.usernameInput,
+                password: this.state.passwordInput
+            })
+        })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.log("Error logging in, ", error))
+    }
+
+    render() {
+        return (
+            <div className="page-wrapper">
+                <Navbar />
+
+                <div className="content-wrapper">
+                    <h2>Login</h2>
+                    <form onSubmit={this.handleSubmit}>
+                        <input 
+                            type="text"
+                            name="usernameInput"
+                            value={this.state.usernameInput}
+                            onChange={this.handleChange}
+                            placeholder="Username"
+                        />
+                        <input 
+                            type="password"
+                            name="passwordInput"
+                            value={this.state.passwordInput}
+                            onChange={this.handleChange}
+                            placeholder="Password"
+                        />
+                        <button type="submit">Submit</button>
+                    </form>
+                </div>
+
+                <Footer />
+            </div>
+        )
+    }
+}
